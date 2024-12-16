71c647c7defcd937ff33981071342f226befc2f4
Mayank Sharna <mayank.sharm90@gmail.com>
2024-12-16 16:55:49 +0530

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Use a base Python image
+FROM python:3.12
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy your project files into the container
+COPY . .
+
+# Install Python dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Expose any necessary ports (optional)
+EXPOSE 8080
+
+# Define the default command to run your DLT pipeline
+# CMD ["python", "pipeline.py"]
---
+++ pipeline.py
@@ -0,0 +1,25 @@
+import dlt
+from dlt.sources.sql_database import sql_database
+
+
+
+def load_tables_family_and_genome():
+    # Create a dlt source that will load tables "family" and "genome"
+    source = sql_database().with_resources("employees", "departments")
+
+    # Create a dlt pipeline object
+    pipeline = dlt.pipeline(
+        pipeline_name="mysql_to_duckdb_pipeline",  # Custom name for the pipeline
+        destination="duckdb",  # dlt destination to which the data will be loaded
+        dataset_name="mysql_to_duckdb_pipeline_data",  # Custom name for the dataset created in the destination
+    )
+
+    # Run the pipeline
+    load_info = pipeline.run(source)
+
+    # Pretty print load information
+    print(load_info)
+
+
+if __name__ == '__main__':
+    load_tables_family_and_genome()
